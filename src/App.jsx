08f39a664a0bbac9f859e74b72a87b5ce372f526
devmohidul <mohidulhasan145@gmail.com>
2025-12-17 22:23:27 +0600

    Cinerental Starts and HTML to JSX some file

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Header from "./Header";
+import Sidebar from "./Sidebar";
+
+export default function App() {
+  return (
+    <div>
+      <Header />
+      <main>
+        <div class="container grid lg:grid-cols-[218px_1fr] gap-[3.5rem]">
+          <Sidebar />
+        </div>
+      </main>
+    </div>
+  );
+}
